be310fc7a3045efc38cc94d0be5df0cb5d4970a0
Eder Costa <eder@encantos.co>
2022-10-02 08:28:38 -0300

    calculate radians by angle

--- src/math.js
+++ src/math.js
@@ -2,12 +2,33 @@ import SpellMathCache from './memory-cache'
 
 export default class SpellMath {
     static getRandomInt(min, max) {
+        if(typeof min !== 'number' || typeof max !== 'number'){
+            throw new Error('SPELL: getRandomInt parameters must be numbers')
+        }
+
+        if(min > max){
+            throw new Error('SPELL: min must be less or equal max')
+        }
+
         min = Math.ceil(min)
         return Math.floor(Math.random() * (Math.floor(max) - min + 1)) + min
     }
 
     static getRandomElement(array){
         return array[this.getRandomInt(0, (array.length - 1))]
+    }
+
+    static getRadians(angle){
+        const id = `${angle}-radian`
+
+        const cached = SpellMathCache.get(id)
+        if(cached){
+            return cached
+        }
+        
+        const op = angle * Math.PI / 180
+
+        return SpellMathCache.add(id, op)
     }
 
     static percentualOf(percent, ofValue){
